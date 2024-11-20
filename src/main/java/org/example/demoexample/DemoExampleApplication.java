3fc7b21a7d5e55f01d64eb43e0efc7919cb11318
Moondy217 <moondaye0217@gmail.com>
2024-11-20 17:25:29 +0900

    chore: test settings

---
+++ src/main/java/org/example/demoexample/DemoExampleApplication.java
@@ -0,0 +1,13 @@
+package org.example.demoexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/demoexample/DemoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demoexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
